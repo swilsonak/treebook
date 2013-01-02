1dd352a21060386897dfdd53eba8abaf8300322c
Shaun Wilson <mr.bologna@gmail.com>
2013-01-01 17:43:58 -0900

    Install devise, add a user, and install devise views

--- db/migrate/20130102023950_add_user_id_to_statuses_table.rb
+++ db/migrate/20130102023950_add_user_id_to_statuses_table.rb
@@ -1,4 +1,4 @@
-class AddUserIdToStatuses < ActiveRecord::Migration
+class AddUserIdToStatusesTable < ActiveRecord::Migration
   def change
   		add_column :statuses, :user_id, :integer
   		add_index :statuses, :user_id
